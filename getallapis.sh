d5043ea46d4798b81f05791f8682b26ba4697d1a
adamr@tmscorp.com <adam.ray@mcleodsoftware.com>
2025-08-15 16:04:56 -0500

    update api import with new specs

--- getallapis.sh
+++ getallapis.sh
@@ -1,3 +1,3 @@
 
-./getapi.sh ordercreation https://innovationhub.mcleodsoftware.com/apis/67856ef1a2155c0001168043/documentation/raw
-./getapi.sh trackandtrace https://blobdevhubassets001.blob.core.windows.net/assets/server-trackandtrace.yaml
+./getapi.sh ordercreation https://innovationhub-assets.mcleodsoftware.com/server-ordercreation.yaml
+./getapi.sh trackandtrace https://innovationhub-assets.mcleodsoftware.com/server-trackandtrace.yaml
